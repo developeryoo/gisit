722e960473c1dc5b9405b7a45efac875b6cda12e
developeryoo <77091347+developeryoo@users.noreply.github.com>
2021-03-21 08:51:15 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const map = new mapgl.Map('section', {
+    center: [129.716087, 62.037494],
+    zoom: 10,
+    key: '4063dfb1-1a15-4509-a767-c396afc8ae3b',
+});
+
+const marker = new mapgl.Marker(map, {
+    coordinates: [55.31878, 25.23584],
+});
